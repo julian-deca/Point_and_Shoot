0d0db0c27446790bfd87a360433d83cd5c0aebdf
julian <juliandecastelli@gmail.com>
2022-10-06 13:00:45 -0300

    improve animation

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const frameSpeed = 5;
 let ravens = [];
 class Raven {
   constructor() {
-    this.flapSpeed = Math.round(Math.random() * 6);
+    this.spriteWidth = 271;
+    this.spriteHeight = 194;
+    this.sizeModifier = Math.random() * 0.6 + 0.4;
+    this.height = this.spriteHeight * this.sizeModifier;
+    this.width = this.spriteWidth * this.sizeModifier;
     this.frames = 0;
-    this.height = Math.random() * 125 + 70;
-    this.width = this.height * 1.4;
     this.x = WIDTH;
     this.y = Math.random() * (HEIGHT - this.height);
     this.image = new Image();
@@ -22,21 +24,26 @@ class Raven {
     this.directionX = Math.random() * 5 + 3;
     this.directionY = Math.random() * 5 + -2.5;
     this.markedForDeletion = false;
-    this.spriteWidth = 271;
-    this.spriteHeight = 194;
+    this.frame = Math.round(Math.random() * 6);
+    this.maxFrame = 4;
+    this.timeSinceFlap = 0;
+    this.flapInterval = Math.random() * 50 + 50;
   }
-  update() {
+  update(deltatime) {
     this.x -= this.directionX;
     if (this.x < 0 - this.width) this.markedForDeletion = true;
-    this.frames++;
-    if (this.frames % frameSpeed === 0) {
-      this.flapSpeed == 5 ? (this.flapSpeed = 0) : this.flapSpeed++;
+
+    this.timeSinceFlap += deltatime;
+    if (this.timeSinceFlap > this.flapInterval) {
+      if (this.frame > this.maxFrame) this.frame = 0;
+      else this.frame++;
+      this.timeSinceFlap = 0;
     }
   }
   draw() {
     ctx.drawImage(
       this.image,
-      this.spriteWidth * this.flapSpeed,
+      this.spriteWidth * this.frame,
       0,
       this.spriteWidth,
       this.spriteHeight,
@@ -57,7 +64,7 @@ function animate(timestamp) {
     ravens.push(new Raven());
     timeToNextRaven = 0;
   }
-  [...ravens].forEach((object) => object.update());
+  [...ravens].forEach((object) => object.update(deltatime));
   [...ravens].forEach((object) => object.draw());
   ravens = ravens.filter((obj) => !obj.markedForDeletion);
 
